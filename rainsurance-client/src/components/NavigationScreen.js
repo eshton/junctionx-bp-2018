3c3f93abf16c40b3e9ed80845d001b73dc6fcd39
Adam Pelle <adam.pelle@skyscanner.net>
2018-10-21 09:47:04 +0200

    added action loading spinner

--- rainsurance-client/src/components/NavigationScreen.js
+++ rainsurance-client/src/components/NavigationScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { PureComponent } from 'react';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import ActionButton from 'react-native-action-button';
 import { Ionicons } from '@expo/vector-icons';
@@ -15,24 +15,47 @@ const styles = StyleSheet.create({
   }
 });
 
-const NavigationScreen = ({ children, navigation: { navigate }, nextScreen, action }) => (
-  <View style={styles.container}>
-    {children}
-    <ActionButton
-      buttonColor='rgb(255, 102, 0)'
-      onPress={async () => {
-        if (action) {
-          await action();
-        }
+class NavigationScreen extends PureComponent {
 
-        navigate(nextScreen);
-      }}
-      style={styles.cta}
-      renderIcon={() => <Ionicons size={25} name="md-arrow-round-forward" color='#fff' />}
-    >
-    </ActionButton>
-  </View>
-);
+  state = {
+    inProgress: false,
+  };
+
+  executeAction = () => {
+    const { navigation: { navigate }, nextScreen, action } = this.props;
+
+    this.setState({ inProgress: true }, async () => {
+      if (action) {
+        await action();
+      }
+
+      this.setState({ inProgress: false }, () => navigate(nextScreen));
+    })
+  };
+
+  render() {
+    const { children, navigation: { navigate }, nextScreen, action } = this.props;
+
+    return (
+      <View style={styles.container}>
+        {children}
+        <ActionButton
+          buttonColor='rgb(255, 102, 0)'
+          onPress={this.executeAction}
+          style={styles.cta}
+          renderIcon={() => {
+            if (this.state.inProgress) {
+              return <ActivityIndicator size="small" color="#fff" />;
+            } else {
+              return <Ionicons size={25} name="md-arrow-round-forward" color='#fff' />;
+            }
+          }}
+        >
+        </ActionButton>
+      </View>
+    );
+  }
+}
 
 export default withNavigation(NavigationScreen);
 
